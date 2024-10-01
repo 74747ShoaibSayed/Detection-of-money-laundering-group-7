c1a55fb25070fc8f50c3af832ee3b8922d9c4694
67611_shoaib <syedshoaibali184@gmail.com>
2024-10-01 20:09:36 +0530

    cdac project

--- main.py
+++
@@ -1,7 +0,0 @@
-from website import create_app #create_app function  imported from website module
-                               #this function configure flask app
-
-app = create_app()# created flask app instance and assigned to app variable
-
-if __name__ == '__main__':#if script is being executed directly or imported as module
-    app.run(debug=True)  #this line starts flask development server
